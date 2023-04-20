9f39fdc016ddbd427d4cbbc37a6f2b26edf2b522
cindy correa <cindycorrea04@gmail.com>
2023-04-20 15:25:58 -0600

    completed api test

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-
-// const Logger = require('./logger');
-// const logger = new Logger;
-
-// // Register a listener
-// logger.on('messageLogged', (arg) => {
-//     console.log('Listener Called!', arg);
-// });
-
-// logger.log('ANYTHING!!!');
-
 const http = require('http');
+
+var name = 'Cindy Correa';
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.write('Hello, Beautiful!')
+        res.write(`This is my name: ${name}`)
         res.end();
     }
 
-    if (req.url === '/api/courses') {
-        res.write(JSON.stringify([1, 2, 3]));
+    if (req.url === '/sayhello') {
+        res.write(`Hello, ${name}! Welcome to my server!`)
         res.end();
     }
 });
