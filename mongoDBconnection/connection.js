2e717c8fbb4a1b02fd72c306743c70db4ff4dbc5
cindy correa <cindycorrea04@gmail.com>
2024-01-16 20:11:14 -0700

    updated render url

--- mongoDBconnection/connection.js
+++ mongoDBconnection/connection.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const URI =
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@lastclass.ishw9yl.mongodb.net/test`;
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@lastclass.ishw9yl.mongodb.net`;
 
 const connectDB = async () => {
   await mongoose.connect(URI);
-  console.log("DB has been connected! Boom baby!");
+  console.log("Wow, I actually did it! MongoDB connected!");
 };
 
 module.exports = connectDB;
