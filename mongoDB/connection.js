0744a4b515693d5679a8819a5921507ed2ae0e11
cindy correa <cindycorrea04@gmail.com>
2024-01-25 13:39:50 -0700

    trying again

--- mongoDB/connection.js
+++ mongoDB/connection.js
@@ -2,14 +2,16 @@ const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv").config();
 
 async function connectDB() {
+  console.log('Please work')
 
   const URI = process.env.URI;
   const client = new MongoClient(URI);
 
   try {
     await client.connect();
+    console.log('It worked!');
   } catch (error) {
-    console.error("There was an error", error);
+    console.error("Hey we are not doing great here.", error);
   }
   return client;
 }
