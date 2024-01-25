945376c7e367701bf4536179a10e7a7eaf6b2a2b
cindy correa <cindycorrea04@gmail.com>
2024-01-25 13:47:22 -0700

    not the client.

--- controller/contacts.js
+++ controller/contacts.js
@@ -4,7 +4,7 @@ const ObjectId = require("mongodb").ObjectId;
 const listAllContacts = async (request, response) => {
   // Connect to MongoDB
   const client = await mongoDB.connectDB();
-  console.log(client);
+  console.log('Something else that is not the client.');
 
   try {
     // Assign collection location
