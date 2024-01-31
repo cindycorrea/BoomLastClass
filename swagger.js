1520069efc9ad8231ad936c32799c7a27ab38839
cindy correa <cindycorrea04@gmail.com>
2024-01-31 13:51:52 -0700

    update schemes

--- swagger.js
+++ swagger.js
@@ -5,7 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'localhost:3000'
+  host: 'localhost:3000',
+  schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
