adfffa8c815a7b1d1bbb72dd1396299f78d2fd8c
cindy correa <cindycorrea04@gmail.com>
2024-02-02 11:27:00 -0700

    permissions

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // Express is a framework to run a node.js API on
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 // start express
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
 // get the information in the MongoDB connection file
-const mongoDB = require('./mongoDB/connection');
+const mongoDB = require("./mongoDB/connection");
 
 // Run on Render's provided port OR
 // port 3000 on my localhost
@@ -20,17 +20,22 @@ const port = process.env.PORT || 3000;
 
 // import routes mini application
 // app.use('/', require('./routes'));
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/', require('./routes/contacts'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/", require("./routes/contacts"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use((request, response, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-type, Accept, Z-Key"
+  );
+  response.setHeader('Content-Type', 'application/json');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+}); 
 
 // Establish a connection to the port nodemon watches for changes
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
